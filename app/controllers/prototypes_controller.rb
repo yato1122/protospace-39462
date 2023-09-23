266f0d3dfe57b586c6e4bdf54ae1610858fae5ff
yato1122 <nandeyanenaiueo@gmail.com>
2023-09-23 22:12:29 +0900

    修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -34,7 +34,7 @@ end
 def update
   @prototype = Prototype.find(params[:id])
 if @prototype.update(prototype_params)
-  redirect_to root_path
+  redirect_to prototype_path
 else
   render :edit
 end
